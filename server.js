388147f45c6b38b9c30dc30865db37bc635dfe6c
Mtnman <billman.designs@gmail.com>
2019-02-05 15:34:36 -0500

    before tuesday class

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3080;
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on localhost:" + PORT);
+});
